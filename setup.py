7b76286316df2fbbe9a4afa613e2f2be90defe97
snow4060 <luohaoyun4068@students.esuhsd.org>
2024-06-27 09:57:26 -0700

    change import path and add long description content type

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -9,11 +9,12 @@ setup(
   version = '1.0.0',
   license='GPL-3.0',
   description = 'An object-oriented wrapper for the KIPR Botball library with additional functionalities.',
+  long_description_content_type="text/markdown",
   long_description=long_description,
   author = 'snow4060',
   author_email = 'haoyun807963@gmail.com',
   url = 'https://github.com/snow4060',
-  download_url = 'https://github.com/ihsrobotics/IHSBotballKit/archive/refs/tags/1.0.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ihsrobotics/IHSBotballKit/archive/refs/tags/1.0.0.tar.gz',
   keywords = ['KIPR', 'BOTBALL', 'WOMBAT', 'LIBKIPR', 'LIBWALLABY'],
   install_requires=[
           'numpy',
@@ -23,7 +24,7 @@ setup(
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License v3.0 (GPL-3.0)',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Programming Language :: Python :: 3',
   ],
 )
